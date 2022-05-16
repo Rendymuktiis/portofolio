d91e8dbbd9578dd358f4893e324e23749cd8de36
Rendymuktiis <rendytrading06@gmail.com>
2022-05-16 16:30:38 +0700

    Updates

---
+++ precache-manifest.7d38abd9a116071772a15339d7d29ff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2689eabd70c8d8e41e81556760013b43",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "1d026e167bacdd8c2934",
+    "url": "/portofolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "28329cc4b804d724f647",
+    "url": "/portofolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "1d026e167bacdd8c2934",
+    "url": "/portofolio/static/js/2.23afcb42.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portofolio/static/js/2.23afcb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28329cc4b804d724f647",
+    "url": "/portofolio/static/js/main.5a58b1ed.chunk.js"
+  },
+  {
+    "revision": "134aa36bd3cf64231d46",
+    "url": "/portofolio/static/js/runtime-main.1d66a8fd.js"
+  }
+]);
